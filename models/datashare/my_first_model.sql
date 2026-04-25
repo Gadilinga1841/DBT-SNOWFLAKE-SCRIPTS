9f021fb0faa83a228c3408312fed2a19bf4473fe
Gadilinga1841 <122167501+Gadilinga1841@users.noreply.github.com>
2026-04-25 16:19:19 +0530

    Update my_first_model.sql

--- models/datashare/my_first_model.sql
+++ models/datashare/my_first_model.sql
@@ -1,13 +1,7 @@
 {% set var='manju' %}
 {{
     config(
-        schema= 'public',
-        pre_hook=["{{set_warehouse(var('wareh'))}}","insert into edw_dev_db1.edw_dev_sc1.aut_lo(model_name,start_time) values('my_first_model',current_timestamp())"],
-        post_hook=[
-            "{{sus_warehouse(var('wareh'))}}",
-            "{{set_warehouse(var('default_wh'))}}",
-            "update edw_dev_db1.edw_dev_sc1.aut_lo set end_time=current_timestamp(),cnt=(select count(*) from {{this}})"
-        ]
+        schema= 'public'
     )
 }}
 
